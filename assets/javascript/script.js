437cae28aff0c8f8452ea2685bbfe33124b1d1f0
Chandler <chndlr.wllm@gmail.com>
2017-09-03 10:42:05 -0500

    updated html, started javascript

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+var topics = ["pizza", "tacos", "beer", "nachos", "BBQ"];
+
+// displayGIFs function re-renders the HTML to display the appropriate content
+
+function displayGIFs() {
+
+  var topics = $(this).attr("data-name");
+  var queryURL = 
+
+
+
+}
