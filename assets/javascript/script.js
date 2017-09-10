f1b3c33841f899be67aaeb5df0f364d7bb265eae
Chandler <chndlr.wllm@gmail.com>
2017-09-10 13:07:51 -0500

    cleaned up js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,61 +2,6 @@
 
 var comida = ["pizza", "bacon", "tacos", "sandwich", "queso", "nachos", "BBQ", "cheese", "sausage", "cereal"];
 
-// displayGIFs function re-renders the HTML to display the appropriate content
-
-function displayGIFs() {
-
-  $("#gifs-view").empty();
-
-  var grub = $(this).attr("data-name");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + grub + "&api_key=b4b3b21c358b416d81cfdb99df5b34fd&limit=10";
-
-  // Create AJAX call for specific grub button being clicked
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-
-    for (var j = 0; j < 10; j++) {
-
-      // Creating a div to hold the GIFs
-      var gifDiv = $("<div class='col-md-5 rect'>");
-
-      // Retrieving the GIFs
-      var gifImage = response.data[j].images.fixed_height_still.url;
-      var gifAction = response.data[j].url;
-
-      // Creating an element to hold the GIFs
-
-      // var gifDisplay = $("<img class='epicGIF'>").attr("src", gifImage);
-
-      var gifDisplay = $("<img>").attr({
-        "src": gifImage,
-        "data-still": gifImage,
-        "data-animate": gifAction,
-        "data-state": "still",
-        "class": "epicGIF"
-        });
-
-      // Append the GIF
-      gifDiv.append(gifDisplay);
-
-      // Storing the rating data
-      var rating = response.data[j].rating;
-      // Creating an element to display rating
-      var elem1 = $("<p>").text("Rating: " + rating.toUpperCase());
-      // Displaying the rating
-      gifDiv.append(elem1);
-
-      // Putting the GIF div above the previous GIFs
-      $("#gifs-view").prepend(gifDiv);
-
-    }
-
-  });
-
-}
-
 // This function renders buttons for all the items in the array
 function renderButtons() {
 
@@ -85,10 +30,66 @@ function renderButtons() {
 
 }
 
+// This function displays GIFs and ratings based on button clicked
+function displayGIFs() {
+  
+    $("#gifs-view").empty();
+  
+    var grub = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + grub + "&api_key=b4b3b21c358b416d81cfdb99df5b34fd&limit=10";
+  
+    // Create AJAX call for specific grub button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+  
+      for (var j = 0; j < 10; j++) {
+  
+        // Creating a div to hold the GIFs
+        var gifDiv = $("<div class='col-md-5 rect'>");
+  
+        // Retrieving the GIFs
+        var gifImage = response.data[j].images.fixed_height_still.url;
+        var gifAction = response.data[j].url;
+        
+  
+        // Creating an element to hold the GIFs
+  
+        // var gifDisplay = $("<img class='epicGIF'>").attr("src", gifImage);
+  
+        var gifDisplay = $("<img>").attr({
+          "src": gifImage,
+          "data-still": gifImage,
+          "data-animate": gifAction,
+          "data-state": "still",
+          "class": "epicGIF"
+          });
+        console.log(gifAction);
+  
+        // Append the GIF
+        gifDiv.append(gifDisplay);
+  
+        // Storing the rating data
+        var rating = response.data[j].rating;
+        // Creating an element to display rating
+        var elem1 = $("<p>").text("Rating: " + rating.toUpperCase());
+        // Displaying the rating
+        gifDiv.append(elem1);
+  
+        // Putting the GIF div above the previous GIFs
+        $("#gifs-view").prepend(gifDiv);
+  
+      }
+  
+    });
+  
+}
+
 // This function starts/stops GIFs on click
 function changeState() {
   console.log(this);
-  // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  // Grab the data-state of clicked image
   var state = $(this).attr("data-state");
   // If the clicked image's state is still, update its src attribute to what its data-animate value is.
   // Then, set the image's data-state to animate
@@ -122,6 +123,7 @@ $("#add-grub").on("click", function(event) {
 // Add a click event listener to all elements with class "grub"
 $(document).on("click", ".grub", displayGIFs);
 
+// Add a click event listener to all elements with class "epicGIF"
 $(document).on("click", ".epicGIF", changeState);
 
 // Call renderButtons function to display the initial buttons
